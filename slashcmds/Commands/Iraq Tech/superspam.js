404f7735fd033d2f7f8ed5201c4a45de999b6c87
pedrohcs8 <pedrohenriquecsena@gmail.com>
2023-01-23 01:09:16 -0400

    Iraq tech update

---
+++ slashcmds/Commands/Iraq Tech/superspam.js
@@ -0,0 +1,47 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  PermissionFlagsBits,
+  ChatInputCommandInteraction,
+} = require('discord.js')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('superspam')
+    .setDescription('Spam de mensagens de cria')
+    .setDMPermission(false)
+    .addNumberOption((options) =>
+      options
+        .setName('quantidade')
+        .setDescription('Quantidade de vezes que o bot vai spamar')
+        .setRequired(true)
+        .setMinValue(1)
+    )
+    .addStringOption((options) =>
+      options
+        .setName('frase')
+        .setDescription('O Texto que será spamado')
+        .setRequired(true)
+    ),
+
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interaction
+   */
+
+  async execute(interaction) {
+    const { options } = interaction
+
+    const quantidade = options.getNumber('quantidade')
+    const frase = options.getString('frase')
+
+    interaction.reply('Super spam has started!')
+
+    for (var i = 0; i < quantidade; i++) {
+      console.log(i)
+      interaction.channel
+        .send({ content: `${frase}` })
+        .then((x) => console.log(i--))
+    }
+  },
+}
