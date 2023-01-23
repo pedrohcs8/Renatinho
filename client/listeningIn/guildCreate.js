385207cb0b4f334f13d4e043e9cdba06eca24d01
pedrohcs8 <pedrohenriquecsena@gmail.com>
2023-01-23 01:26:30 -0400

    guild Create Repairs

--- client/listeningIn/guildCreate.js
+++ client/listeningIn/guildCreate.js
@@ -1,66 +1,71 @@
-const { MessageEmbed } = require("discord.js");
+const { EmbedBuilder } = require('discord.js')
 
 module.exports = class {
   constructor(client) {
-    this.client = client;
+    this.client = client
   }
 
   async run(guild) {
     try {
-      const JOIN = new MessageEmbed()
+      const JOIN = new EmbedBuilder()
         .setTimestamp()
         .setColor(process.env.EMBED_COLOR)
-        .setThumbnail(guild.iconURL({ dynamic: true, size: 2048 }));
+        .setThumbnail(guild.iconURL({ dynamic: true, size: 2048 }))
 
-      const INVITE = await guild.invites.fetch()
+      const INVITE = await guild.invites
+        .fetch()
         .then((invites) => {
-          if (invites) return invites.random().url;
-          return false;
+          if (invites) return invites.random().url
+          return false
         })
         .catch(() => {
-          return false;
-        });
+          return false
+        })
 
-      if (INVITE) JOIN.setURL(INVITE).setTitle(guild.name);
+      if (INVITE) JOIN.setURL(INVITE).setTitle(guild.name)
 
       const owner = await guild.fetchOwner()
 
-        JOIN.setAuthor(`${guild.client.user.username} | Adicionado à um Servidor`).addFields(
-          {
-            name: `Nome do Servidor`,
-            value: `${guild.name}`,
-            inline: true,
-          },
-          {
-            name: `ID Do Servidor`,
-            value: `${guild.id}`,
-            inline: true,
-          },
-          {
-            name: `Propietário`,
-            value: `${owner}`,
-            inline: true,
-          },
-          {
-            name: `ID Do Propietário`,
-            value: `${owner.id}`,
-            inline: true,
-          },
-          {
-            name: `Total de Usuários`,
-            value: `${guild.memberCount}`,
-            inline: true,
-          },
-          {
-            name: `Total de Canais`,
-            value: `${guild.channels.cache.size}`,
-            inline: true,
-          }
-        )
+      JOIN.setAuthor(
+        `${guild.client.user.username} | Adicionado à um Servidor`
+      ).addFields(
+        {
+          name: `Nome do Servidor`,
+          value: `${guild.name}`,
+          inline: true,
+        },
+        {
+          name: `ID Do Servidor`,
+          value: `${guild.id}`,
+          inline: true,
+        },
+        {
+          name: `Propietário`,
+          value: `${owner}`,
+          inline: true,
+        },
+        {
+          name: `ID Do Propietário`,
+          value: `${owner.id}`,
+          inline: true,
+        },
+        {
+          name: `Total de Usuários`,
+          value: `${guild.memberCount}`,
+          inline: true,
+        },
+        {
+          name: `Total de Canais`,
+          value: `${guild.channels.cache.size}`,
+          inline: true,
+        }
+      )
 
-      this.client.channels.cache.get(process.env.CHANNEL_LOGS).send({ embeds: [JOIN] })
+      this.client.channels.cache
+        .get(process.env.CHANNEL_LOGS)
+        .send({ embeds: [JOIN] })
     } catch (err) {
-      console.log(`EVENTO: GuildCreate ${err}`);
+      console.log(`EVENTO: GuildCreate ${err}`)
     }
-  };
+  }
 }
