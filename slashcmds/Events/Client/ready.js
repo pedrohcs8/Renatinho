5e0c86e77b75bcf8bacdd07fa3eed32a7e114072
pedrohcs8 <pedrohenriquecsena@gmail.com>
2023-09-22 22:49:01 -0400

    Custom Status

--- slashcmds/Events/Client/ready.js
+++ slashcmds/Events/Client/ready.js
@@ -1,4 +1,4 @@
-const { Client } = require('discord.js')
+const { Client, ActivityType } = require('discord.js')
 const { loadCommands } = require('../../Handlers/Commands')
 
 module.exports = {
@@ -19,16 +19,18 @@ module.exports = {
     //Status custom aleatório
     setInterval(() => {
       const statuses = [
-        `Criado por pedrohcs8#4185`,
+        `Criado por pedrohcs8`,
         `Em desenvolvimento`,
         `Com host :)`,
         `Renato online!`,
+        `Meu prefixo padrão é .`,
         `Hospedado em um Raspberry Pi 4!`,
         `Online - Cluster 1-Renato-Host`,
+        `Use .help se necessário`,
       ]
 
       const status = statuses[Math.floor(Math.random() * statuses.length)]
-      client.user.setActivity(status, { type: 'PLAYING' })
+      client.user.setActivity(status, { type: ActivityType.Playing })
     }, 5000)
 
     console.log('Os slashcommands estão prontos!')
