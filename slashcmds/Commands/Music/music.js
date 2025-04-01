cc040ec05d5b6c03914bf0567b48c984c14c3f76
pedrohcs8 <pedrohenriquecsena@gmail.com>
2025-03-31 23:24:33 -0400

    oops again

--- slashcmds/Commands/Music/music.js
+++ slashcmds/Commands/Music/music.js
@@ -381,7 +381,7 @@ module.exports = {
           const textChannel = options.getChannel('canal-texto')
           const voiceChannel = options.getChannel('canal-voz')
 
-          if (!voiceChannel.isDMBased()) {
+          if (!textChannel.isDMBased()) {
             return interaction.reply('O canal selecionado não é de texto')
           }
 
